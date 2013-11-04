caf6c084351f3711e2835fce371793e57e630d1c
Gulsah Kandemir Yildiz <fgulsahkandemir@gmail.com>
2013-11-04 14:31:25 -0800

    Initial Commit
    The initial configuration of the folders created by "rails new funmap"
    command

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Funmap::Application.config.secret_key_base = 'feaefb6ae56491778265f591e69d4cdd7d1e54c6e6ca64758f4edcd9d69e02b000c05bff3a081a3e5be8938ed36d6ab43f7a9ef6e335453cfea325b0adfd1a16'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Funmap::Application.config.session_store :cookie_store, key: '_funmap_session'
