8326f6e38f141bde6343f592a4a09dfa722b846d
Gulsah Kandemir Yildiz <fgulsahkandemir@gmail.com>
2013-11-16 02:03:25 -0800

    Infopopover and filterbox updated.

--- app/assets/javascripts/fm.filterbox.js
+++ app/assets/javascripts/fm.filterbox.js
@@ -2,26 +2,45 @@
 $.widget('fm.fm_filterbox', {
     options: {
     },
+    eles: {
+        collapseHandler: null,
+        panelCollapse: null,
+        moreCategoriesDiv: null,
+        moreButton: null
+    },
     _create: function() {
-    	console.log("fm_filterbox create");
-    	this.element.find(".panel-collapse.collapse.in").
-    		on('hide.bs.collapse',$.proxy(this._hideCollapsable,this));
-    	this.element.find(".panel-collapse.collapse").
-    		on('show.bs.collapse',$.proxy(this._showCollapsable,this));
+        this._getUiEles();
 
+    	this.eles.panelCollapse
+    		.on('hide.bs.collapse', $.proxy(this._hideCollapsable,this))
+            .on('show.bs.collapse', $.proxy(this._showCollapsable,this));
+    	this.eles.moreButton.on("click",$.proxy(this._toggleMoreCategories,this));
     },
     _destroy: function() {
-    	console.log("fm_filterbox destroy");
+    },
+    _getUiEles: function() {
+        this.eles.collapseHandler = this.element.find(".js-collapse-handler");
+        this.eles.panelCollapse = this.element.find(".panel-collapse");
+        this.eles.moreCategoriesDiv = this.element.find(".js-more-categories");
+        this.eles.moreButton = this.element.find(".js-more");
     },
     _hideCollapsable: function(){
-    	this.element.find(".glyphicon.glyphicon-chevron-down").
-    		addClass("glyphicon-chevron-right").
-    		removeClass("glyphicon-chevron-down");
+    	this.eles.collapseHandler
+    		.addClass("glyphicon-chevron-right")
+    		.removeClass("glyphicon-chevron-down");
     },
     _showCollapsable: function(){
-    	this.element.find(".glyphicon.glyphicon-chevron-right").
-    		addClass("glyphicon-chevron-down").
-    		removeClass("glyphicon-chevron-right");
+    	this.eles.collapseHandler
+    		.addClass("glyphicon-chevron-down")
+    		.removeClass("glyphicon-chevron-right");
     },
-
+    _toggleMoreCategories: function(){
+    	this.eles.moreCategoriesDiv.slideToggle();
+        var buttonText = this.eles.moreButton.text();
+        if(buttonText == "more"){
+            this.eles.moreButton.text("less").blur();
+        } else {
+            this.eles.moreButton.text("more").blur();
+        }
+    }
 });
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,28 +1,20 @@
 class EventsController < ApplicationController
 
 	def index
+		template_string = File.open("app/views/events/_info_popover.html.erb").read
+		template = ERB.new(template_string)
 		@feeds = Feed.find(:all, :conditions => ['date >= ?', Date.today])
 		@hash = Gmaps4rails.build_markers(@feeds) do |feed,marker|
 			marker.lat feed.latitude
 			marker.lng feed.longitude
 			marker.json({:id => feed.id })
-			marker.infowindow generate_info_window_html(feed)
+			marker.infowindow template.result(binding)
 		end
 
-		@marker_lat = Feed.first.latitude
-		@marker_lng = Feed.first.longitude
-		@marker_title = Feed.first.title
-	end
-
-	def generate_info_window_html(feed)
-		html = ""
-		html << "<a href=\"" << feed.id_feed << "\" target=\"_blank\">" << feed.title << "</a><br>" 
-		if !feed.img_src.nil?
-			html << "<img src=\"" << feed.img_src << "\" height=\"150\" align=left>"
-		end
-		if !feed.summary.nil?
-			html << "<p>" + feed.summary[0..100] + "</p>"	
-		end
-		return html
+		@top_categories = Category.joins(:feed_has_categories)
+									.select('categories.id, categories.title, count(*) as "feed_count"')
+									.group(:category_id)
+									.order('feed_count desc')
+									.to_a[0..8]
 	end
 end
