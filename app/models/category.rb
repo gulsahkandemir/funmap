7d568ffa5ab322410fb7fe96be6c1746f594a27f
Gulsah Kandemir Yildiz <fgulsahkandemir@gmail.com>
2013-11-23 13:05:31 -0800

    Irrelevant categories are excluded from filters

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
 	has_many :feed_has_categories
 	has_many :feeds, through: :feed_has_categories
+
+    def self.excluded_category_ids
+        [13,18,22]
+    end
 end
